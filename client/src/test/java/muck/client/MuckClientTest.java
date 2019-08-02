74964e32e0ebbb552be8e3bb0e490722e2c17731
William Billingsley <wbilling@une.edu.au>
2019-08-02 10:45:45 +1000

    Removed broken test

--- client/src/test/java/muck/client/MuckClientTest.java
+++ client/src/test/java/muck/client/MuckClientTest.java
@@ -6,9 +6,9 @@ package muck.client;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+class MuckClientTest {
+    @Test void testsRun() {
+        //This test does not check anything useful  
+        assertTrue(true);
     }
 }
