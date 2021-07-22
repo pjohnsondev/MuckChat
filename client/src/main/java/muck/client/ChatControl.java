7af721a54af8bb426756491720027b7a50c2e720
Chantelle Perreau <cperreau@myune.edu.au>
2021-07-22 16:02:29 +1000

    Chat box now accepts text and spits it out

--- client/src/main/java/muck/client/ChatControl.java
+++ client/src/main/java/muck/client/ChatControl.java
@@ -1,18 +1,53 @@
 package muck.client;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
+
 
 public class ChatControl implements Initializable {
 
+    @FXML
+    TextArea groupChatBox;
+
+    @FXML
+    TextField messageBox;
+
+    @FXML
+    Button enter;
+
+    String message;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        enter.setOnAction(this::buttonActionGroup);
+
     }
-}
+
+    private void buttonActionGroup(ActionEvent event) {
+        message = messageBox.getText();
+        if (message.length() != 0) {
+            groupChatBox.appendText(message + "\n");
+            messageBox.clear();
+        }
+    }
+        @FXML
+        private void onEnter(ActionEvent event) {
+            message = messageBox.getText();
+            if (message.length() != 0) {
+                groupChatBox.appendText(message + "\n");
+                messageBox.clear();
+
+            }
+        }
+
+    }
+
+
