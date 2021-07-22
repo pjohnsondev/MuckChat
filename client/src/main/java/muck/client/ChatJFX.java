3d8a0af1c9e960d0b0fdacad265b755005f639b4
Chantelle Perreau <cperreau@myune.edu.au>
2021-07-22 16:02:29 +1000

    Chat box now excepts text and spits it out

--- client/src/main/java/muck/client/ChatJFX.java
+++ client/src/main/java/muck/client/ChatJFX.java
@@ -1,16 +1,33 @@
 package muck.client;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
+import javafx.event.EventHandler;
 
 
 public class ChatJFX extends Application {
 
-        @Override
+    @FXML
+    TextArea groupChatBox;
+
+    @FXML
+    TextField messageBox;
+
+    @FXML
+    Button enter;
+
+    String message;
+
+
+    @Override
         public void start(Stage primaryStage) throws Exception {
             try {
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MuckChat.fxml"));
@@ -25,7 +42,7 @@ public class ChatJFX extends Application {
                 e.printStackTrace();
 
             }
-        }
+    }
 
             public static void main (String[]args){
                 launch(args);
