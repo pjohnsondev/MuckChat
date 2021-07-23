e55ffd809c3c64654fb2bc7cd3b5f13c3f936f34
Ryan <rbirch4@myune.edu.au>
2021-07-23 15:55:01 +1000

    Got the client to send a message to the server and the server can print it out. Using custom userMessage class.

---
+++ network/src/main/java/muck/protocol/connection/userMessage.java
@@ -0,0 +1,15 @@
+package muck.protocol.connection;
+
+public class userMessage {
+    private String message;
+
+
+    public void setMessage(String messageIn) {
+        this.message = messageIn;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+}
