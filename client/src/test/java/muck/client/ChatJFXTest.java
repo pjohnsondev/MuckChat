3a80a7bb03ab6d8f548ffed8e68284fdef2d642d
Chantelle Perreau <cperreau@myune.edu.au>
2021-07-23 11:01:02 +1000

    Empty ChatJFX Test class.

---
+++ client/src/test/java/muck/client/ChatJFXTest.java
@@ -0,0 +1,36 @@
+package muck.client;
+import javafx.fxml.LoadException;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class ChatJFXTest {
+
+    private static final Logger logger = LogManager.getLogger(ChatJFXTest.class);
+
+
+    @Test void chatLaunches() {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
