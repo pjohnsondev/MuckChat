e430592baee2f8c095f551d059e3a09820132a6e
Matt Trippe <mtrippe@turing.une.edu.au>
2021-07-26 12:46:55 +1000

    creating a core file for our dice roll gameplay mechanic

---
+++ client/src/main/java/muck/client/GameMechanics.java
@@ -0,0 +1,15 @@
+package muck.client;
+package aw.character;
+
+public class GameMechanics {
+
+    /** Current field in use in branch 9 to piggyback off **/
+    // private int_health = 100;
+
+    //private int_strength=1;
+    //private int_mind=1;
+    //private int_dex=1;
+
+}
+
+
