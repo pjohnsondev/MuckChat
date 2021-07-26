9c8b4b0ed7bccc2c7988075536dae96bc05832c3
Ethan Carlsson <ecarlsso@myune.edu.au>
2021-07-26 18:50:54 +0200

    set up database bindings

--- server/src/main/java/muck/server/database/Database.java
+++ server/src/main/java/muck/server/database/Database.java
@@ -2,11 +2,14 @@ package muck.server.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Database {
     private String connectionString;
     private Connection conn;
+    private PreparedStatement statement;
+
     public Database() {
         // Just in case some people are using windows.
         String seperator = java.io.File.separator;
@@ -35,4 +38,52 @@ public class Database {
     public Boolean databaseIsConnected() {
         return conn != null;
     }
+
+    public void query(String sql) {
+        try {
+            statement = conn.prepareStatement(sql);
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    // PREPARED STATEMENT BINDING
+    public void bindInt(int pIndex, int parameter) {
+        try {
+            statement.setInt(pIndex, parameter);
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+    public void bindDouble(int pIndex, double parameter) {
+        try {
+            statement.setDouble(pIndex, parameter);
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+    public void bindString(int pIndex, String parameter) {
+        try {
+            statement.setString(pIndex, parameter);
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+    public void bindNull(int pIndex) {
+        try {
+            statement.setString(pIndex, null);
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public void bindObj(int pIndex, Object parameter) {
+        try {
+            statement.setObject(pIndex, parameter);
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    //END PREPARED STATMENT BINDING
 }
