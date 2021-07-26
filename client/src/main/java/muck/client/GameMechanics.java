280ce05b10d088e609c62355c66c5743f36cda1e
¨Matthew <mtrippe@myune.edu.au>
2021-07-26 14:07:17 +1000

    Added GenRandom for the dice rolls

--- client/src/main/java/muck/client/GameMechanics.java
+++ client/src/main/java/muck/client/GameMechanics.java
@@ -1,15 +1,15 @@
 package muck.client;
-package aw.character;
+//package aw.character;
 
-public class GameMechanics {
+public class GameMechanics{
 
-    /** Current field in use in branch 9 to piggyback off **/
-    // private int_health = 100;
-
+    //private int_health = 100;             //Group9 collab currently
     //private int_strength=1;
     //private int_mind=1;
     //private int_dex=1;
 
 }
+
+
 
 
