4c5e2bd842ca5a20c34ed0bd9a9f89bc3f305898
Peter Kiefel <pkiefel@myune.edu.au>
2021-07-26 20:33:01 +1000

    setup folder structure and start of a test unit.

---
+++ server/src/test/java/muck/server/DataBaseUserTest.java
@@ -0,0 +1,15 @@
+package muck.server;
+
+import org.junit.jupiter.api.Test;
+
+public class DataBaseUserTest {
+    @Test
+    public void dbUserAccessTes(){
+        /*This test unit will ensure the connection to the database is working*/
+    }
+
+    @Test
+    public void requestUserInformationTest(){
+        /*This test unit will test incoming user information testing.*/
+    }
+}
