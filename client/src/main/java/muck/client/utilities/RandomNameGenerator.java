e4d7670c1183507032a048453d4fac5c0dcc2e25
Mark Hay <mhay23@turing.une.edu.au>
2021-07-27 12:22:32 +1000

    Added random name generator

--- client/src/main/java/muck/client/utilities/RandomNameGenerator.java
+++ client/src/main/java/muck/client/utilities/RandomNameGenerator.java
@@ -1,2 +1,14 @@
-package muck.client.utilities;public class RandomNameGenerator {
+package muck.client.utilities;
+
+/*Random Name Generator - Issue 16 */
+//TODO: Random Name Generator - implement functionality
+public class RandomNameGenerator {
+
+    RandomNameGenerator(){
+
+    }
+
+    public String generate(){
+        return "john smith";
+    }
 }
