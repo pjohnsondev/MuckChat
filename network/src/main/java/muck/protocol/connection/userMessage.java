d9eddd4350a48d3e5b112e51dcce144e4a775457
rb2267 <50123701+rb2267@users.noreply.github.com>
2021-07-27 15:17:39 +1000

    Added initial Javadoc comments.

--- network/src/main/java/muck/protocol/connection/userMessage.java
+++ network/src/main/java/muck/protocol/connection/userMessage.java
@@ -1,13 +1,26 @@
 package muck.protocol.connection;
 
+/**
+ * userMessage creates a new instance of a message to be sent in chat.
+ * Initial Author: Ryan Birch (rbirch4) - Low Expectations.
+ * Revision: 1.
+ */
+
 public class userMessage {
     private String message;
 
-
+    /**
+     * setMessage sets the message to be sent.
+     * @param messageIn
+     */
     public void setMessage(String messageIn) {
         this.message = messageIn;
     }
 
+    /**
+     * getMessage can be called to return the string of the message.
+     * @return
+     */
     public String getMessage() {
         return this.message;
     }
