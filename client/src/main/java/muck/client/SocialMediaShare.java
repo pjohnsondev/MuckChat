aec9dda77bc8319d8adcea18e8902db998564737
William hamilton <whamilt6@myune.edu.au>
2021-07-28 09:51:56 +1000

    Added Class for Social Media Sharing functionality.

---
+++ client/src/main/java/muck/client/SocialMediaShare.java
@@ -0,0 +1,7 @@
+package muck.client;
+
+public class SocialMediaShare {
+
+    // TODO: implement functionality that will allow users to post to Social Media
+    //  platforms their progress and/or achievements.
+}
