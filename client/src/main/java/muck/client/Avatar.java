0fc2ddc6a532d64b5d563b7ef53d6c4bbd2bb0ad
Camillie Ashmore <cashmore@myune.edu.au>
2021-07-28 19:00:08 +1000

    XML for Avatar screen, updated repositories and dependencies, created a generic Avatar class to run program

--- client/src/main/java/muck/client/Avatar.java
+++ client/src/main/java/muck/client/Avatar.java
@@ -1,51 +1,32 @@
 package muck.client;
 
 import javafx.application.Application;
-import javafx.scene.layout.*;
-import javafx.scene.*;
+import javafx.fxml.FXMLLoader;
+//import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Avatar extends Application {
 
-    String characterName; // will be passed into the class from 'log in screen'
-    int uniqueId; // not sure if this is needed to be stored here. Will be passed into the class from 'log in screen'
-    int characterAppearance; // Hopefully this will store the selected avatar number
-    private final int width = 100; //size of the character
-    private final int height = 200; //size of character
-
     @Override
-    public void start(Stage stage) throws Exception {
-        final int numCols = 4;
-        final int numRows = 3;
-
-        // Set scene
-        GridPane root = new GridPane();
-        root.setGridLinesVisible(true); //remove this when finished designing
-        Scene scene = new Scene(root, 648, 480);
-
-        // Need to set background
-
-
-
-
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        try {
+            //FXMLLoader loader = new FXMLLoader(ReportMenu.this.getClass().getResource("/fxml/" + report.getClass().getCanonicalName().substring(18).replaceAll("Controller", "") + ".fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Avatar.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            scene.setRoot(root);
+            Stage stage = new Stage();
+            stage.setTitle("Muck 2021");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-    /*public Avatar (int type, String name, int id) {
-        this.characterName = name;
-        this.uniqueId = id;
-
-
-    }
-
-    private void selectPlayer() {
-
-    }*/
-
-
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
