85440730a266d1d149a1b384339e27d58f3c5afa
stevem42 <elevated42@yahoo.com.au>
2021-07-28 13:27:41 +1000

    Added some methods to card and deck classes

--- client/src/main/java/muck/client/card_games/Card.java
+++ client/src/main/java/muck/client/card_games/Card.java
@@ -3,4 +3,42 @@ package muck.client.card_games;
 //TODO: Create card class
 
 public class Card {
+
+    public enum Suit {
+        CLUBS, DIAMONDS, HEARTS, SPADES;
+    }
+
+    private int value;
+    private Suit suit;
+
+
+    public Card(int value, Suit suit) {
+        setValue(value);
+        setSuit(suit);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public static void main(String[] args) {
+        Card card;
+
+        card = new Card(5, Suit.HEARTS);
+
+        System.out.println("TESTING");
+        System.out.println(card.value + " of " + card.suit);
+    }
 }
--- client/src/main/java/muck/client/card_games/Deck.java
+++ client/src/main/java/muck/client/card_games/Deck.java
@@ -2,5 +2,34 @@ package muck.client.card_games;
 
 //TODO: Create deck class
 
+import java.util.ArrayList;
+
 public class Deck {
+    ArrayList<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<Card>();
+        for (int i = 1; i < 14; i++) {
+            cards.add(new Card(i, Card.Suit.HEARTS));
+        }
+        for (int i = 1; i < 14; i++) {
+            cards.add(new Card(i, Card.Suit.SPADES));
+        }
+        for (int i = 1; i < 14; i++) {
+            cards.add(new Card(i, Card.Suit.CLUBS));
+        }
+        for (int i = 1; i < 14; i++) {
+            cards.add(new Card(i, Card.Suit.DIAMONDS));
+        }
+    }
+
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        for (int i = 0; i < deck.cards.size(); i++ ) {
+            System.out.println(deck.cards.get(i).getValue() + " of " +  deck.cards.get(i).getSuit() );
+        }
+
+    }
+
+
 }
