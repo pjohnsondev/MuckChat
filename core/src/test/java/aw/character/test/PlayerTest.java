f755a1c11dfcd5dc48fd0b0a3a2e61ddd95cfbaf
Krisna Gusti <kgusti@myune.edu.au>
2021-07-29 20:28:12 +1000

    Added a player test class & updated player class

---
+++ core/src/test/java/aw/character/test/PlayerTest.java
@@ -0,0 +1,18 @@
+package aw.character.test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PlayerTest {
+
+    private static final Logger logger = LogManager.getLogger(PlayerTest.class);
+
+    // Test Dummy - TO BE REMOVED
+    @Test
+    public void testTestSuiteRuns() {
+        logger.info("Dummy test");
+        assertTrue(true);
+    }
+}
