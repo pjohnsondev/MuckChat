198d7230e3957f04146843159904d2e7bd2faeaa
Ethan Carlsson <ecarlsso@myune.edu.au>
2021-07-30 08:26:30 +0200

    set up a test tfor creating a new table

--- server/src/test/java/muck/server/UserModelTest.java
+++ server/src/test/java/muck/server/UserModelTest.java
@@ -1,7 +1,24 @@
 package muck.server;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import java.sql.SQLException;
+
+import muck.server.models.User;
+import muck.server.testHelpers.TestDatabase;
 
 public class UserModelTest {
+
+    @Test
+    public void testDbCreation() throws SQLException {
+        TestDatabase testDb = new TestDatabase();
+        User user = new User();
+        user.changeDb(testDb);
+
+        user.createTable();
+       
+        assertTrue(testDb.tableExists("users"));
+        user.closeDbConnection();
+    }
     
     @Test
     public void dbUserAccessTest(){
