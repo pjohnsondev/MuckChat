6194277a404cd2ca1ed73a39fc545e71dd55d376
Trent Graw <tgraw@myune.edu.au>
2021-07-30 22:55:52 +1000

    Added suite of tests for hashcodes, equality and internal structure for location/pair/triple

--- core/src/test/java/muck/core/TripleTests.java
+++ core/src/test/java/muck/core/TripleTests.java
@@ -2,21 +2,72 @@ package muck.core;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.ArrayList;
 
 public class TripleTests {
-    @Test
-    void TestGetters() {
-	var x = "String";
-	var y = 3;
-	var z = new ArrayList<Integer>();
-	z.add(1);
-	z.add(2);
-	z.add(3);
-	var triple = new Triple<String, Integer, ArrayList<Integer>>(x,y,z);
+	@Test
+	void TestGetters() {
+		var x = "String";
+		var y = 3;
+		var z = 'C';
 
-	assertEquals(x, triple.left());
-	assertEquals(y, triple.middle());
-	assertEquals(z, triple.right());
+		var triple = new Triple(x, y, z);
+
+		assertEquals(x, triple.left());
+		assertEquals(y, triple.middle());
+		assertEquals(z, triple.right());
+	}
+
+	@Test
+	void PairsWithDifferentValuesHaveDifferentHashcodes() {
+
+		var triple = new Triple(1, 2, 3).hashCode();
+		var triple2 = new Triple(4, 5, 6).hashCode();
+
+		assertNotEquals(triple, triple2);
+	}
+
+	@Test
+	void TriplesWithSameValueshaveSameHashcodes() {
+
+		var triple = new Triple(1, 2, 3).hashCode();
+		var triple2 = new Triple(1, 2, 3).hashCode();
+
+		assertEquals(triple, triple2);
+	}
+
+	@Test
+	void TriplesWithInverseValuesHaveDifferentHashcodes() {
+		var triple = new Triple(1, 2, 3).hashCode();
+		var triple2 = new Triple(3, 2, 1).hashCode();
+
+		assertNotEquals(triple, triple2);
+	}
+
+	@Test
+	void TriplesWithSameValuesAreEqual() {
+
+		var triple = new Triple(1, 2, 3);
+		var triple2 = new Triple(1, 2, 3);
+
+		assertEquals(triple, triple2);
+	}
+
+	@Test
+	void TriplesWithInverseValuesAreNotEqual() {
+
+		var triple = new Triple(1, 2, 3);
+		var triple2 = new Triple(3, 2, 1);
+
+		assertNotEquals(triple, triple2);
+	}
+
+    @Test
+	void TriplesUpdatedProduceExpectedTriples() {
+
+		var triple = new Triple(1, 2, 3).updateLeft(2).updateMiddle(3).updateRight(4);
+		var expected = new Triple(2, 3, 4);
+
+		assertEquals(expected, triple);
+		assertEquals(expected.hashCode(), triple.hashCode());
     }
 }
