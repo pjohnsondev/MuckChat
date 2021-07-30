21d04d294f3e8a9c66cc0f80b91d5421d1f0f41e
Ethan Carlsson <ecarlsso@myune.edu.au>
2021-07-30 08:28:45 +0200

    User model can now create a table with 'createTable(); can close connection in Model with 'closeDbConnection'

--- server/src/main/java/muck/server/models/Model.java
+++ server/src/main/java/muck/server/models/Model.java
@@ -1,10 +1,21 @@
 package muck.server.models;
 
+import muck.server.database.Database;
 import muck.server.database.MuckDatabase;
 
 public class Model {
-    private MuckDatabase db;
+    protected Database db;
     public Model() {
         db = new MuckDatabase();
     }
+
+    public void changeDb(Database db) {
+        closeDbConnection();
+        this.db = db;
+    }
+
+    public void closeDbConnection() {
+        db.closeConnection();
+    }
+
 }
--- server/src/main/java/muck/server/models/User.java
+++ server/src/main/java/muck/server/models/User.java
@@ -1,8 +1,32 @@
 package muck.server.models;
 
+import java.sql.SQLException;
 
-public class User extends Model {
+public class User extends Model{
 
+    private String username;
     private byte[] hashedPassword;
+    private byte[] salt;
+
+    public void createTable() throws SQLException {
+        // create a new table
+        // this conditional is used to emulate the "CREATE IF NOT EXISTS" syntax from other SQL implementations
+        // this "IF NOT EXISTS" doesn't exist in Apache Derby.
+        if (!db.tableExists("users")) {
+            db.query(
+                "CREATE TABLE users "
+                + "(id INTEGER UNIQUE GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
+                + " username VARCHAR(80) UNIQUE, "
+                + " password BLOB NOT NULL, "
+                + " salt BLOB NOT NULL)"
+                );
+    
+            db.execute();    
+        }
+    }
+
+    public void registerNewUser(String username, String password) throws SQLException {
+
+    }
 
 }
