37099d4050d65f33e308b9ed76ece243cdf40639
Alyssa McAulay <amcaulay@myune.edu.au>
2021-07-31 20:50:19 +1000

    Added Player Class for Go fish

---
+++ client/src/main/java/muck/client/card_games/Player.java
@@ -0,0 +1,27 @@
+package muck.client.card_games;
+
+import java.awt.*;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+public class Player {
+    private int score;
+    ArrayList<Card> sets;
+    ArrayList<Card> hand;
+
+    public Player(){
+        this.score = 0;
+        hand = new ArrayList<Card>();
+        sets = new ArrayList<Card>();
+
+    }
+
+    public static void main(String[] args) {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        System.out.println("Testing players. Player 1's score: " + player1.score
+                + ". Player 2's score: " + player2.score);
+
+    }
+
+}
