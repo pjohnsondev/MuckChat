f2cf007a296eccea7a81020e6d00e24270607a1e
Jason Aboh <jaboh@myune.edu.au>
2021-07-31 21:13:52 +1000

    corrected gradle wrapper version to 7.1.1 , Added SignInTest , added controller Folder and SignInController class

---
+++ client/src/main/java/muck/client/controllers/SignInController.java
@@ -0,0 +1,16 @@
+package muck.client.controllers;
+/*Sign in - Issue 11 */
+public class SignInController {
+
+//TODO: Sign in validation method - implement functionality
+
+// Create a constructor to allow access to class.
+public SignInController(){
+
+        }
+
+        public String validateSignIn(){
+            return "True";
+        }
+    }
+
