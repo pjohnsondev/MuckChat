7172ecdd3c6414fd00cbc3648d2ef0fec2667507
Alyssa McAulay <amcaulay@myune.edu.au>
2021-08-01 10:51:42 +1000

    Added function to add to score

--- client/src/main/java/muck/client/card_games/Player.java
+++ client/src/main/java/muck/client/card_games/Player.java
@@ -16,12 +16,19 @@ public class Player {
 
     }
 
+    void add_score(){
+        this.score += 10;
+    }
+
     public static void main(String[] args) {
         Player player1 = new Player();
         Player player2 = new Player();
         System.out.println("Testing players. Player 1's score: " + player1.score
                 + ". Player 2's score: " + player2.score);
-
+        player1.add_score();
+        player2.add_score();
+        System.out.println("Testing adding to score function. Player 1's score: "
+                + player1.score + ". Player 2's score: " + player2.score);
     }
 
 }
