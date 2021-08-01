f91573b6491dcad1bace5660486cf3c13b8086c6
Ashley Travaini <atravain@myune.edu.au>
2021-08-01 17:17:58 +1000

    Listener for client disconnect added, however client app must call disconnect

--- client/src/main/java/muck/client/App.java
+++ client/src/main/java/muck/client/App.java
@@ -79,6 +79,7 @@ public class App extends Application {
             // Just demonstrates that the worker manager hands the ping off -- likely to be removed when the project
             // progresses
             MuckClient.INSTANCE.send(new Ping());
+            //MuckClient.INSTANCE.disconnect(); // This successfully disconnects client but does it immediately
         } catch (IOException ex) {
             logger.error("Start up failed");
         }
@@ -86,8 +87,14 @@ public class App extends Application {
 
 
     void shutdown() {
-        // Exit the program
-        System.exit(0);
+        try {
+            MuckClient.INSTANCE.disconnect();
+            System.exit(0);
+            logger.info("Client disconnected successfully");
+        } catch (IOException ex) {
+            System.exit(1);
+            logger.error("Client exited without disconnecting");
+        }
     }
 
     public static void main(String[] args) {
--- server/src/main/java/muck/server/MuckServer.java
+++ server/src/main/java/muck/server/MuckServer.java
@@ -56,11 +56,18 @@ public enum MuckServer {
         kryoServer.bind(config.getTcpPort(), config.getUdpPort());
 
         // The arraylist is only a temporary datastructure and is subject to change.
-        ArrayList<Integer> players = new ArrayList<Integer>();
+        ArrayList<String> players = new ArrayList<String>();
         // Adds a listener to listen for new client connections, then adds the clients id to the players arraylist.
-        addListener(ListenerBuilder.forClass(Connected.class).onReceive((conn, client) -> {
-            players.add(conn.getID());
+        addListener(ListenerBuilder.forClass(Connected.class).onReceive((conn, connected) -> {
+            players.add(Integer.toString(conn.getID()));
             logger.info("Player connection id's are: {}", players);
+
+        }));
+
+        // Adds a listener to listen for clients disconnecting from the server, then removes them from the players arraylist.
+        addListener(ListenerBuilder.forClass(Disconnect.class).onReceive((conn, disconnect) -> {
+            players.remove(Integer.toString(conn.getID())); // This will obtain the index of the player
+            logger.info("Player connection id's are: {} disconnected: {}", players, disconnect);
         }));
 
         // Add a Ping listener. Still being used for debugging.
