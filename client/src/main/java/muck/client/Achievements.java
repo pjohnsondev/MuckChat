7cb5c377b72c4c1e1800d81651b5cfe50897de9e
Jesse Chow <jchow5@myune.edu.au>
2021-08-11 21:25:09 +1000

    First iteration of the achievement ass

---
+++ client/src/main/java/muck/client/Achievements.java
@@ -0,0 +1,36 @@
+package muck.client;
+
+import javax.swing.*;
+
+/*
+This class stores and triggers achievements when specific in-game requirements are met.
+*/
+
+public class Achievements {
+
+    // List of achievements that can be earned in-game
+    // NEED TO ADD ACHIEVEMENTS HERE
+    public boolean achievement1 = false;
+    public String achievement1Title = "Achievement 1";
+    public String achievement1Description = "This is the description for Achievement 1";
+
+
+
+    //
+    public void achievementEarned(boolean achievementName) {
+        if (achievementName == false) {
+            achievementName = true;
+            JTextField achievementNameNotification = new JTextField("Achievement Unlocked!", 30);
+            // NOT SURE IF THIS WORKS...
+        }
+    }
+
+    public void achievementQuery(boolean achievementName) {
+        if (achievementName == true) {
+            System.out.print("The achievement " + achievementName + " has already been unlocked!");
+        }
+        else if (achievementName == false) {
+            System.out.print("The achievement " + achievementName + " has not yet been unlocked!");
+        }
+    }
+}
