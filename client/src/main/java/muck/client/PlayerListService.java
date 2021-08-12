be33b9b02e141510d5a952a0b1e0452d347d6c57
Ashley Travaini <atravain@myune.edu.au>
2021-08-12 19:06:19 +1000

    Added comments to the PlayerListService

--- client/src/main/java/muck/client/PlayerListService.java
+++ client/src/main/java/muck/client/PlayerListService.java
@@ -2,34 +2,29 @@ package muck.client;
 
 import javafx.scene.control.TextArea;
 import javafx.concurrent.ScheduledService;
-import javafx.concurrent.*;
 import javafx.application.Platform;
-import javafx.event.EventTarget;
-import javafx.concurrent.Worker.State;
-import muck.client.MuckController;
-import javafx.application.Application;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.concurrent.ScheduledService;
 import javafx.concurrent.Task;
-import javafx.concurrent.WorkerStateEvent;
-import javafx.event.EventHandler;
-import javafx.stage.Stage;
-import javafx.util.Duration;
 
+/*
+PlayerListService is used to create a async task that can be run off the javafx thread allowing
+the functionality needed to consistently update and display the list of current players.
+ */
 class PlayerListService extends ScheduledService<Void> {
 
     private TextArea textBox;
 
+    // The constructor needed to pass the playerTextArea from the Muck controller into the service
     public PlayerListService(TextArea someTextBox) {
         this.textBox = someTextBox;
     }
 
+    // Creates the async task and assign it
     @Override
     protected Task<Void> createTask(){
         return new Task<>() {
             @Override
             protected Void call() {
+                // Updates the Player list in the GUI
                 Platform.runLater(() -> {
                     fillPlayerList(textBox);
                 });
@@ -38,6 +33,9 @@ class PlayerListService extends ScheduledService<Void> {
         };
     }
 
+    /* fillPlayerList takes a textArea and clears it, then reads from the current instance
+    of the client to iterate over the stored playerlist to add each player to the text area
+    */
     public void fillPlayerList(TextArea someTextBox) {
         someTextBox.clear();
         for (String player: MuckClient.INSTANCE.players) {
