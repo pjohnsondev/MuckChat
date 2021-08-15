c83b1474e4bcf00069466eb2d3fc7cda62523c23
Kiri Lefebvre <klefebvr@myune.edu.au>
2021-08-15 18:41:14 +1000

    Updated HelperTest.java to wrap common code into beforeEach function

--- server/src/test/java/muck/server/HelperTest.java
+++ server/src/test/java/muck/server/HelperTest.java
@@ -1,6 +1,12 @@
 package muck.server;
 
 import muck.server.helpers.security.*;
+import muck.server.testHelpers.TestDatabase;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -8,15 +14,27 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class HelperTest {
+    private static final Logger logger = LogManager.getLogger(HelperTest.class);
+
+    private Hasher hasher;
+    private String password;
+
+    /**
+     * Set up environment before each test
+     */
+    @BeforeEach
+    public void beforeEach() {
+        logger.info("This message prints BEFORE each test runs");
+        hasher = new Hasher();
+        password = "password";
+        hasher.setNewPasswordHash(password);
+    }
 
     /**
      * Test that password is generated
      */
     @Test
     public void generateNewPasswordTest() {
-        Hasher hasher = new Hasher();
-        String password = "password";
-        hasher.setNewPasswordHash(password);
 
         assertTrue(hasher.getSalt() != null);
         assertTrue(hasher.getHashedPassword() != null);
@@ -27,9 +45,6 @@ public class HelperTest {
      */
     @Test
     public void passwordMatchesTest() {
-        Hasher hasher = new Hasher();
-        String password = "password";
-        hasher.setNewPasswordHash(password);
         byte[] salt = hasher.getSalt();
         byte[] hashedPassword = hasher.getHashedPassword();
 
