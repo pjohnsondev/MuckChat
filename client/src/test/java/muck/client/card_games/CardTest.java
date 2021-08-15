dfdda9ac803c96e8d4b6325338dfc79e840bd4b4
Steve Mckinnon <smckin27@myune.edu.au>
2021-08-16 09:49:42 +1000

    refactored card filenames

--- client/src/test/java/muck/client/card_games/CardTest.java
+++ client/src/test/java/muck/client/card_games/CardTest.java
@@ -6,6 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+// Some tests to write
+
+/*
+    Test that the same card can't exist in more than one place (could use id as it's unique)
+    Test that the length of the hands and the deck is accurate in certain situations, like after dealing, after picking from the pile etc.
+    Test that each card has an image - not null. (Once it's been implemented)
+
+ */
+
 public class CardTest {
 
     private static final Logger logger = LogManager.getLogger(Deck.class);
