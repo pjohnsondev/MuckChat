b1839eed45c26a167104397dbb214d71d49be9c9
Mark Hay <mhay23@myune.edu.au>
2021-08-20 11:55:23 +1000

    Added interaction controller class

---
+++ client/src/main/java/muck/client/utilities/InteractionController.java
@@ -0,0 +1,34 @@
+package muck.client.utilities;
+
+import muck.protocol.connection.userMessage;
+
+// INTERACT WITH GAME UI TO INITIATE INTERACTION
+
+// IDENTIFY USER BEING INTERACTED WITH
+
+// GENERATE INTERACTION TYPE
+
+// SEND MESSAGE TO CHAT UI
+
+public class InteractionController {
+
+    userMessage um = new userMessage();
+
+
+    InteractionController(){
+        um.setMessage("This is a test Message");
+    };
+
+    public void sendToChat(userMessage um){
+
+    }
+
+
+
+
+
+
+
+
+
+}
