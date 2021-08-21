a9e0598d2de2381777534d2f39e5a83eb0e38132
Steve Mckinnon <smckin27@myune.edu.au>
2021-08-21 11:44:04 +1000

    Commented Card Class

--- client/src/main/java/muck/client/card_games/Card.java
+++ client/src/main/java/muck/client/card_games/Card.java
@@ -1,61 +1,103 @@
 package muck.client.card_games;
-import javafx.application.Application;
 import javafx.scene.image.Image;
-import javafx.stage.Stage;
 
-//TODO: Create card class
-
+/**
+ * Card Class. Instantiates an individual regular playing card.
+ */
 public class Card{
-
-    /*@Override
-        public void start(Stage primaryStage) throws Exception{
-
-    }*/
-
-
-
     private int id;
     private String suit, cardName;
+    private Image cardImage;
+    private String fileName;
 
-
+    /**
+     * Constructor Function for the Card Class
+     * Sets the Cards Id, Suit and Card Name
+     * Sets the image file location, and the individual Card Image
+     * @param id
+     * @param suit
+     * @param cardName
+     */
     public Card(int id, String suit, String cardName) {
         setValue(id);
         setSuit(suit);
         setCardName(cardName);
+        fileName = "/images/cards/" + cardName + "_of_" + suit.toLowerCase() + ".png";
+        cardImage = new Image(fileName);
     }
 
+    /**
+     * getValue Method.
+     * Returns the ID of any Card. Each Card has a unique idea between 1 and 52
+     * @return id
+     */
     public int getValue() {
         return id;
     }
 
+    /**
+     * getSuit Method.
+     * Returns the name of the suit for an individual card
+     * @return suit
+     */
     public String getSuit() {
         return suit;
     }
 
+    /**
+     * getCardName Method.
+     * Returns the individual card name as a string. from "two" through to "ace"
+     * @return cardName
+     */
+    public String getCardName() {
+        return cardName;
+    }
+
+    /**
+     * getCardImage Method.
+     * Returns the Image of an individual card. Connected through the filename - path to the card image.
+     * @return cardImage
+     */
+    public Image getCardImage() {
+        return cardImage;
+    }
+
+    /**
+     * setValue Method.
+     * Sets the unique card ID as an Int
+     * @param id
+     */
     public void setValue(int id) {
         this.id = id;
     }
 
+    /**
+     * setSuit Method.
+     * Sets the individual card suit name as a String
+     * @param suit
+     */
     public void setSuit(String suit) {
         suit = suit.toLowerCase();
         this.suit = suit;
     }
 
-    public String getCardName() {
-        return cardName;
-    }
-
+    /**
+     * setCardName Method.
+     * Sets the individual card name as a lowercase string. From "two" through to "ace"
+     * @param cardName
+     */
     public void setCardName(String cardName) {
         cardName = cardName.toLowerCase();
         this.cardName = cardName;
     }
 
-    public static void main(String[] args) {
-        Card card;
 
-        card = new Card(5, "Hearts", "5");
-
-        System.out.println("TESTING");
-        System.out.println(card.cardName + " of " + card.suit);
-    }
+//    public static void main(String[] args) {
+//        Card card;
+//
+//        card = new Card(5, "Hearts", "5");
+//
+//        System.out.println("TESTING");
+//        System.out.println(card.cardName + " of " + card.suit);
+//    }
 }
--- client/src/main/java/muck/client/card_games/Deck.java
+++ client/src/main/java/muck/client/card_games/Deck.java
@@ -1,26 +1,22 @@
 package muck.client.card_games;
 
-//TODO: Create deck class
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
+
 public class Deck {
     ArrayList<Card> cards;
     String[] Suits = {"clubs", "diamonds", "hearts", "spades"};
     String[] cardNames = {"ace","2","3","4","5","6","7","8","9","10","jack","queen","king"};
-    String cardValue;
-    String cardName;
     String suit;
     int counter;
     Image backOfDeck;
 
 
-
     public Deck() {
-
         cards = new ArrayList<Card>();
         Image backOfDeck = new ImageIcon("images/cards/backofdeck.png").getImage();
         counter = 0;
@@ -37,27 +33,30 @@ public class Deck {
             else {
                 suit = Suits[3];
             }
-
             cards.add(new Card(i, suit, cardNames[counter]));
             counter += 1;
             if (counter % 13 == 0) {
                 counter = 0;
             }
         }
-
     }
+
+
+    public Image getDeckImage() {
+        return backOfDeck;
+    }
+
+
     public void shuffle_cards() {
         Collections.shuffle(cards);
     }
-    
-    public static void main(String[] args) {
-        Deck deck = new Deck();
-        for (int i = 0; i < deck.cards.size(); i++ ) {
-            System.out.println(deck.cards.get(i).getCardName() + " of " +  deck.cards.get(i).getSuit() );
-            System.out.println("This cards ID is " + deck.cards.get(i).getValue());
-        }
-
-    }
 
 
+//    public static void main(String[] args) {
+//        Deck deck = new Deck();
+//        for (int i = 0; i < deck.cards.size(); i++ ) {
+//            System.out.println(deck.cards.get(i).getCardName() + " of " +  deck.cards.get(i).getSuit() );
+//            System.out.println("This cards ID is " + deck.cards.get(i).getValue());
+//        }
+//    }
 }
