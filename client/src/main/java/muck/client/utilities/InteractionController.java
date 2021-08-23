843515b24a88d2b4bcdfcb137524c68d87ea4e38
Mark Hay <mhay23@myune.edu.au>
2021-08-23 17:45:05 +1000

    Interaction controller class - non functional

--- client/src/main/java/muck/client/utilities/InteractionController.java
+++ client/src/main/java/muck/client/utilities/InteractionController.java
@@ -1,6 +1,14 @@
 package muck.client.utilities;
 
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
+import muck.client.App;
 import muck.protocol.connection.userMessage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 // INTERACT WITH GAME UI TO INITIATE INTERACTION
 
@@ -12,16 +20,36 @@ import muck.protocol.connection.userMessage;
 
 public class InteractionController {
 
-    userMessage um = new userMessage();
+    private static final Logger logger = LogManager.getLogger(InteractionController.class);
 
+    private userMessage um = new userMessage();
+    private TextArea guiChatBox;
+    private String defaultMessage = "Default Interaction Controller Message";
 
-    InteractionController(){
-        um.setMessage("This is a test Message");
+    InteractionController(Scene scene, TextArea guiChatBox){
+        logger.info("Interaction controller Instantiated");
+        um.setMessage(defaultMessage);
+        this.guiChatBox = guiChatBox;
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getText() == "m"){
+                    sendToChat();
+                }
+            }
+        });
     };
 
-    public void sendToChat(userMessage um){
-
+    public void sendToChat(){
+        guiChatBox.appendText(um.getMessage());
     }
+
+
+
+    //receive reference to chat GUI chat box
+
+    //TextArea guiChatBox = getChatBox();
 
 
 
