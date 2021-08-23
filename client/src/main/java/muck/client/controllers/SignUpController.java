32b66ac7df513c9dc58c4c75bdc2dd0356ee7fc4
Jason Aboh <jaboh@myune.edu.au>
2021-08-23 16:50:48 +1000

    Move database and models to core as instructed

--- client/src/main/java/muck/client/controllers/SignUpController.java
+++ client/src/main/java/muck/client/controllers/SignUpController.java
@@ -3,22 +3,21 @@ package muck.client.controllers;
 
 
 import javafx.event.ActionEvent;
-import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.text.Text;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import muck.client.AvatarController;
-import org.mindrot.jbcrypt.*;
-import muck.client.AvatarController;
+import muck.client.App;
+import muck.core.models.models.UserModel;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.io.IOException;
-import java.security.InvalidParameterException;
 import java.sql.SQLException;
-import muck.client.App;
 
 
 public class SignUpController {
