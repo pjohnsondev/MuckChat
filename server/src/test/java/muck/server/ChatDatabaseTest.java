8b1202108ed822eb03341a5f289417a38b91c945
Peter John Kiefel <pkiefel@myune.edu.au>
2021-08-25 17:55:24 +1000

    Started Database functionality for chat feature

---
+++ server/src/test/java/muck/server/ChatDatabaseTest.java
@@ -0,0 +1,21 @@
+package muck.server;
+
+import org.junit.jupiter.api.Test;
+
+public class ChatDatabaseTest {
+    @Test
+    public void retrieveGroupChatMessagesTest() {
+
+    }
+
+    @Test
+    public void addMessagesTest() {
+
+    }
+
+    @Test
+    public void retrieveIndividualChatsTest() {
+
+    }
+
+}
