ac28537e04ae5b9b0d6c9aa2511699a3f9e34949
Steven Buchanan <sbuchan9@myune.edu.au>
2021-08-26 13:33:04 +1000

    database structure test

---
+++ server/src/main/java/muck/server/structures/PlayerObject.java
@@ -0,0 +1,11 @@
+package muck.server.structures;
+
+public class PlayerObject {
+    public String identifier = null;
+
+    public String health = null;
+    public String attack = null;
+    public String defense = null;
+
+
+}
