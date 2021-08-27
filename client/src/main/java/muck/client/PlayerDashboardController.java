1638958eba49f5eb6d8dd86c4d667b5de9b0d540
Camillie Ashmore <cashmore@myune.edu.au>
2021-08-27 15:19:28 +1000

    Updated layout of playerdashboard window

--- client/src/main/java/muck/client/PlayerDashboardController.java
+++ client/src/main/java/muck/client/PlayerDashboardController.java
@@ -8,28 +8,28 @@ import java.util.logging.Level;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Circle;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class PlayerDashboardController implements Initializable {
     private static String userName;
-    private static String avatarID = "peach";
+    private static String avatarID;
     private Image fullAvatar = AvatarController.getFullAvatar(avatarID);
-    private int muckPointTotal = 15;
-    private int healthTotal = 80;
+    private int muckPointTotal = 100; //TODO: Remove
+    private int healthTotal = 80; //TODO: Remove
 
+    @FXML
+    private GridPane gridPane;
 
     @FXML
     private ImageView avatarFullBody;
@@ -49,11 +49,16 @@ public class PlayerDashboardController implements Initializable {
     @FXML
     private ImageView gameReturn;
 
+    private final BackgroundImage background = new BackgroundImage(new Image("/images/BackgroundAvSelection.jpg"), null, null, null, null);
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
             avatarFullBody.setPreserveRatio(true);
+            gridPane.setBackground(new Background(background));
             avatarFullBody.setImage(fullAvatar);
+            selection(avatarID);
             centreImage();
             username.setText(userName);
             muckPoints.setText(String.valueOf(muckPointTotal));
@@ -105,18 +110,6 @@ public class PlayerDashboardController implements Initializable {
         userName = uname;
         avatarID = avID;
         //TODO: Call the server to get all the relevant information
-        /*try {
-            Parent root = FXMLLoader.load(PlayerDashboardController.class.getResource("/fxml/PlayerDashboard.fxml"));
-            Scene scene = new Scene(root);
-            scene.setRoot(root);
-            scene.getStylesheets().add(PlayerDashboardController.class.getResource("/css/style.css").toExternalForm());
-            //This line gets the Stage Information
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException ex) {
-            java.util.logging.Logger.getLogger(AvatarController.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
     }
 
     public static void playerDashboard(String uname, MouseEvent event, String avID) {
@@ -169,7 +162,40 @@ public class PlayerDashboardController implements Initializable {
         }
     }
 
-    public static void main(String[] args) {
-        PlayerDashboardController.playerDashboard("Test");
+    private void selection(String character) {
+        try {
+            switch (character) {
+                case "peach":
+                    avatarFullBody.setLayoutY(90.0);
+                    avatarFullBody.setFitHeight(390);
+                    break;
+                case "batman":
+                    avatarFullBody.setLayoutY(40.0);
+                    break;
+                case "pikachu":
+                    avatarFullBody.setLayoutY(210.0);
+                    avatarFullBody.setFitHeight(250);
+                    break;
+                case "skeleton":
+                    avatarFullBody.setLayoutY(120.0);
+                    avatarFullBody.setFitHeight(410);
+                    break;
+                case "wonderWoman":
+                    avatarFullBody.setLayoutY(70.0);
+                    avatarFullBody.setFitHeight(400);
+                    break;
+                case "yoshi":
+                    avatarFullBody.setLayoutY(180.0);
+                    avatarFullBody.setFitHeight(300);
+                    break;
+                case "error":
+                    break;
+                default:
+                    break;
+            }
+            centreImage();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
