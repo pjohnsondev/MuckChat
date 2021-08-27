0a34579abd9932adb142715aee427459a5ff73fe
¨Matthew <mtrippe@myune.edu.au>
2021-08-27 23:02:47 +1000

    Adding class retainers and pseudo code

---
+++ client/src/main/java/muck/client/enduring_fantasy/Character.java
@@ -0,0 +1,16 @@
+package muck.client.enduring_fantasy;
+
+public abstract class Character {
+    private String charName;
+    private int maxHp = 0;
+    private int maxDmg = 0;
+
+    public Character(){ }
+
+    public String getCharName() {return this.charName;}
+    //public int getHealth() {return this.maxHp;}
+    //public int getDamage() {return this.maxDmg;}
+
+    //public void damage?
+
+}
---
+++ client/src/main/java/muck/client/enduring_fantasy/Monster.java
@@ -0,0 +1,36 @@
+package muck.client.enduring_fantasy;
+import muck.client.Dice;
+
+public class Monster extends Character {
+    private Dice monster = new Dice(1);
+    private String monsDesc = " ";
+    private int dmgtotal = 0;
+
+    public Monster() {
+    }
+
+    public void genMonster(int num) {
+        int monsGen = this.monster.nextInt(4);
+        int monsHealth = num * 100;
+        int monsDmg = num * 10;
+        if (monsGen <= 1) {
+            this.setName("Rat");
+            this.setHealth(monsHealth + 1);
+            this.setDmg(monsDmg);
+            this.monsDesc = "A large rat";
+        } else if (monsGen == 3) {
+            this.setName("Wolf");
+            this.setHealth(monsHealth + 50);
+            this.setDmg(monsDmg + 20);
+            this.monsDesc = "A lone feral wolf";
+        } else {
+            this.setName("Bear");
+            this.setHealth(monsHealth + 100);
+            this.setDmg(monsDmg + 150);
+            this.monsDesc = "A large Bear");
+        }
+
+    }
+
+}
+
