bcad927011253b42294be15ed0c98adb01cf4273
nedmond3 <nedmond3@myune.edu.au>
2021-08-28 19:47:45 +1000

    Added Classes for implementing and testing sprite distances

---
+++ client/src/main/java/muck/client/utilities/SpriteDistance.java
@@ -0,0 +1,10 @@
+package muck.client.utilities;
+import muck.client.Sprite;
+//TODO: IMPLEMENT
+public class SpriteDistance {
+    
+    public double Calculate(Sprite a, Sprite b) {
+        return 1.0; // should return distance between two sprites.
+    }
+
+}
