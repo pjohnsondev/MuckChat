c09d42a4d5d8a9015fcfb5d0f58503865763161c
Krisna Gusti <kgusti@myune.edu.au>
2021-09-03 15:32:53 +1000

    Add Test cases for CatNPC

--- client/src/main/java/muck/client/character_client/CatNPC.java
+++ client/src/main/java/muck/client/character_client/CatNPC.java
@@ -7,11 +7,9 @@ import muck.core.character.NPC;
 
 public class CatNPC extends NPC {
     private final Image image;
-
-    // Variables to select specific cat colour and direction
-    private int base;
-    private int sh;
-    private int sx;
+    private int base; // Top and Bottom Colours (0/192)
+    private int sx; // Colour (0/144/288/432)
+    private int sh; // Direction (48/96/144)
 
     /**
      * Constructor - Position and colour
@@ -48,7 +46,7 @@ public class CatNPC extends NPC {
     }
 
     /**
-     * Set the colour of the cat
+     * Set the colour of the cat. Default is white
      * @param colour Colour of cat (grey, brown, black, beige, tip, tiger, white)
      */
     public void setColour(String colour) {
@@ -86,6 +84,7 @@ public class CatNPC extends NPC {
                 base = 0;
                 break;
         }
+        sh = base;
     }
 
     /**
@@ -99,15 +98,40 @@ public class CatNPC extends NPC {
     }
 
     /**
-     * Set the facing direction of cat
+     * Change the facing direction of cat NPC
      * @param direction New direction
      */
     public void changeDirection(String direction) {
-        switch (direction) {
+        this.setDirection(direction);
+        switch (getDirection()) {
             case "left" -> sh = base + 48;
             case "right" -> sh = base + 96;
             case "up" -> sh = base + 144;
             default -> sh = base;
         }
+    }
+
+    /**
+     * Dialog option for cat NPC
+     * @param option dialog option
+     * @return String of chosen dialog option
+     */
+    public String dialog(int option) {
+        String message;
+        switch (option) {
+            case 1 -> message = "Meow!";
+            case 2 -> message = "Hiss!!!";
+            case 3 -> message = "Purr";
+            default -> message = "...";
+        }
+        return message;
+    }
+
+    /**
+     * Return source rectangle X and Y coordinates
+     * @return Array of source rectangle X and source rectangle Y
+     */
+    public int[] getSourceRectangle() {
+        return new int[]{this.sx, this.sh};
     }
 }
