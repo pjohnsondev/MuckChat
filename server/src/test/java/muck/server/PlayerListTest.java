9b62ad55c08f922fb617cf1fef15ec003106cd5e
Ashley Travaini <atravain@myune.edu.au>
2021-09-04 13:15:42 +1000

    Changes made to playerlist test

--- server/src/test/java/muck/server/PlayerListTest.java
+++ server/src/test/java/muck/server/PlayerListTest.java
@@ -1,22 +1,10 @@
 package muck.server;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryonet.Client;
-import com.esotericsoftware.kryonet.Server;
-import muck.core.ClientId;
-import muck.core.Id;
-import muck.protocol.KryoClientConfig;
-import muck.protocol.KryoServerConfig;
-import muck.protocol.Protocol;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlayerListTest {
-    static KryoServerConfig ServConfig = new KryoServerConfig();
-    static KryoClientConfig config = new KryoClientConfig();
 
     @Test
     // Test that the server contains the playerlist and that we can add to it
