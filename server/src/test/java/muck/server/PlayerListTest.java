8a47a75b78455abccf76ad0f273539c9128042ab
Ashley Travaini <atravain@myune.edu.au>
2021-09-05 12:17:43 +1000

    Added unit test for player list

--- server/src/test/java/muck/server/PlayerListTest.java
+++ server/src/test/java/muck/server/PlayerListTest.java
@@ -1,7 +1,9 @@
 package muck.server;
 
 import com.esotericsoftware.kryonet.Client;
+import muck.core.Login;
 import muck.core.character.Player;
+import muck.core.user.SignUpInfo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -11,7 +13,7 @@ public class PlayerListTest {
 
     @Test
     // Test that the server contains the playerlist and that we can add to it
-    public void playerList() {
+    public void serverContainsPlayerList() {
         Player player = new Player("TestUser");
         Client client = new Client();
         MuckServer muckServer = MuckServer.getINSTANCE();
