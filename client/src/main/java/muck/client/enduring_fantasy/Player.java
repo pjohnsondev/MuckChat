266199e607c0126e8a327b1ab090687166d587d5
¨Matthew <mtrippe@myune.edu.au>
2021-09-06 13:21:41 +1000

    tweaked player

--- client/src/main/java/muck/client/enduring_fantasy/Player.java
+++ client/src/main/java/muck/client/enduring_fantasy/Player.java
@@ -15,16 +15,42 @@ public class Player extends Character {
         this.magicStr = 0;
     }
 
-    public void setNextLvl(int nextLevel) { this.nextLevel = nextLevel;}
+    /** Lets set Magic stats **/
     public int getMagicStr() { return this.magicStr;}
     public void setMagicStr(int mag) { this.magicStr = mag;}
     public int getMP() { return this.magicPoints;}
     public void setMP(int newMP) { this.magicPoints = newMP;}
     public void incMagPoints(int newPoints) { this.magicPoints = this.magicPoints += newPoints;}
 
+    /** Lets set level stats **/
+    public void setNextLvl(int nextLevel) { this.nextLevel = nextLevel;}
+    public int getNextLevel(){ return this.nextLevel;}
+    public void decNextLvl(int newLvl){ this.nextLevel -= newLvl;}
+    public void resetLvl(){ this.nextLevel = this.playerLevel * 150;}
+    public int getPlayerLvl(){ return this.playerLevel;}
+    public void setPlayerLvl(int newLvl){ this.playerLevel = newLvl;}
+
+    public void incPlayLvl() {
+        if (this.nextLevel <= 0) {
+            ++this.playerLevel;
+            System.out.println("You have increased your level");
+        }
+        this.resetLvl();
+
+    } else { System.out.println("Your have gained experience");}
+
+
+
+    /** Lets set up player classes **/
     public String getType() {return this.type;}
     public void setType(String newType){
-        /** FINISH ME TOMARROW - SET MY Job Classes**/
+        /** SET Job Classes **/
     }
+
+
+    public void setTypeStats(){
+        /** set job base stats here **/
+    }
+
 
 }
