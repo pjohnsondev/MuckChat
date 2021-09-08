c6874b88d36d0debfff236c832996b711611b2cc
nbarnfield <nedbarnfield@gmail.com>
2021-09-08 18:26:09 +1000

    Added overloaded method to movedown() for enemy sprites.

--- client/src/main/java/muck/client/space_invaders/SpaceInvaders.java
+++ client/src/main/java/muck/client/space_invaders/SpaceInvaders.java
@@ -2,25 +2,31 @@ package muck.client.space_invaders;
 
 import java.awt.*;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.Random;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import static  java.util.Map.entry;
+import muck.client.space_invaders.SpriteAnimation;
 
 
-//TODO: collision detection, additional levels with additional enemy-medium and enemy-small images,
+// TODO: collision detection, additional levels with additional enemy-medium and enemy-small images,
 // game over page, game completed page, change from mouse events to key events "w.a.s.d.space'
 
 public class SpaceInvaders {
@@ -30,24 +36,39 @@ public class SpaceInvaders {
     private static final int HEIGHT = 800;
     private static final int PLAYER_SIZE = 35;
     private static final int ENEMY_SIZE = 75;
-    private static final Image PLAYER = new Image("/images/spaceinvaders/player-ship.gif");
-    private static final Image PLAYER_LASER = new Image("/images/spaceinvaders/" +
-            "player-laser.gif");
-    private static final Image ENEMY_BIG = new Image
-            ("/images/spaceinvaders/" +
-                    "enemy-big.gif");
-    private static final Image ENEMY_MEDIUM = new Image
-            ("/images/spaceinvaders/enemy-medium.gif");
-    private static final Image ENEMY_SMALL = new Image
-            ("/images/spaceinvaders/enemy-small.gif");
 
+    private static Map<String, String> imageURLs = Map.ofEntries(
+            entry("ENEMY_SMALL","/images/spaceinvaders/enemy-small.gif"),
+            entry("ENEMY_MEDIUM", "/images/spaceinvaders/enemy-medium.gif"),
+            entry("ENEMY_BIG", "/images/spaceinvaders/enemy-big.gif"),
+            entry("PLAYER", "/images/spaceinvaders/player-ship.gif"),
+            entry("PLAYER_LASER", "/images/spaceinvaders/player-laser.gif")
+    );
 
+    private static final Image PLAYER_LASER = new Image(imageURLs.get("PLAYER_LASER"));
 
     private GraphicsContext gc;
 
-    private SISprite player;
+    private  SpriteAnimation player;
 
-    private double mouseX;
+    // Direction key combinations
+    KeyCombination A = new KeyCodeCombination(KeyCode.A);
+    KeyCombination W = new KeyCodeCombination(KeyCode.W);
+    KeyCombination S = new KeyCodeCombination(KeyCode.S);
+    KeyCombination D = new KeyCodeCombination(KeyCode.D);
+    SimpleIntegerProperty directionKeyCount = new SimpleIntegerProperty(0);
+
+
+    // TODO: Only one keyCode is returned at a time, may need to use the mouse to shoot
+    // Shooting key combinations
+//    KeyCombination SPACE = new KeyCodeCombination(KeyCode.SPACE);
+//    KeyCombination ASPACE = new KeyCodeCombination(KeyCode.A, KeyCombination.);
+//    KeyCombination WSPACE = new KeyCodeCombination(KeyCode.W, KeyCode.SPACE);
+//    KeyCombination SSPACE = new KeyCodeCombination(KeyCode.S, KeyCode.SPACE);
+//    KeyCombination DSPACE = new KeyCodeCombination(KeyCode.D,KeyCode.SPACE);
+
+
+    //private double mouseX;
     private int score;
     private int level;
     private int lives;
@@ -55,26 +76,44 @@ public class SpaceInvaders {
 
     List<StarBackground> stars;
     List<PlayerMissiles> playerMissiles;
-    List<Enemy> enemies;
+//    List<Enemy> enemies;
+    List<SpriteAnimation> enemies;
 
     public void start(Stage stage) throws Exception {
-
-
+        // Create animation event loop and background
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         gc = canvas.getGraphicsContext2D();
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(50), e -> run(gc)));
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
-        canvas.setCursor(Cursor.MOVE);
-        canvas.setOnMouseMoved(e -> mouseX = e.getX());
-        canvas.setOnMouseClicked(e ->
-                playerMissiles.add(player.shoot())
-        );
         setup();
-        stage.setScene(new Scene(new StackPane(canvas)));
+
+        // Enable key event detection for standard AWSD configuration and shooting
+        Scene scene = new Scene(new StackPane(canvas));
+        scene.setOnKeyPressed(e -> {
+            // If keys match a direction it will increase the directionKeyCount used to test whether the player
+            // should move in the run method. The AWSD key code is initialised and stored against the SpriteAnimation.
+            // If any other key is pressed it resets the directionKeyCount preventing unexpected behaviour or having
+            // the sprite continue to move as the condition would be true.
+            if(A.match(e) || W.match(e) || S.match(e) || D.match(e)) {
+                player.setDirection(e.getCode());
+                directionKeyCount.set(directionKeyCount.get() + 1);
+            }
+            else {
+                directionKeyCount.set(0);
+            }
+        });
+
+        scene.setOnKeyReleased(e -> {
+            if (A.match(e) || W.match(e) || S.match(e) || D.match(e)) {
+                directionKeyCount.set(0);
+            }
+        });
+
+//        stage.setScene(new Scene(new StackPane(canvas)));
+        stage.setScene(scene);
         stage.setTitle("Muck 2021 Space Invaders");
         stage.show();
-
     }
 
 
@@ -85,11 +124,8 @@ public class SpaceInvaders {
         score = 0;
         level = 1;
         lives = 3;
-        player = new SISprite
-                (WIDTH / 2, HEIGHT -
-                        (PLAYER_SIZE + 40), PLAYER_SIZE,
-                        (PLAYER_SIZE*2), "player", PLAYER);
-
+        player = new SpriteAnimation(imageURLs.get("PLAYER"), PLAYER_SIZE,  (PLAYER_SIZE*2), WIDTH / 2, HEIGHT -
+                (PLAYER_SIZE + 40),true, true, 5, 1, "player");
     }
 
 
@@ -124,8 +160,30 @@ public class SpaceInvaders {
             if (stars.get(i).posY > HEIGHT)
                 stars.remove(i);
         }
-        player.drawSprite();
-        player.x = (int) mouseX - PLAYER_SIZE/2;
+
+        // Draw and move player
+        gc.drawImage(player, player.getX(), player.getY());
+        if(directionKeyCount.get() > 0){
+            switch (player.getDirection()) {
+                case A:
+                    player.moveLeft();
+                    break;
+                case D:
+                    player.moveRight();
+                    break;
+                case W:
+                    player.moveUp();
+                    break;
+                case S:
+                    player.moveDown();
+                    break;
+                // TODO: Add key combination for shooting and movement i.e. ASPACE
+                // TODO: Add shoot method to SpriteAnimation and add it in here as a standalone key combination
+                //case SPACE: player.shoot(); break;
+                // case ESCAPE: pause timer break;
+
+            }
+        }
 
         for (int i = playerMissiles.size() - 1; i >=0 ; i--) {
             PlayerMissiles playerMissile = playerMissiles.get(i);
@@ -137,7 +195,13 @@ public class SpaceInvaders {
             playerMissile.draw();
 
         }
-        enemies.forEach(Enemy::drawEnemy);
+
+
+        // Draw enemy sprites
+        for(int i = 0; i < enemies.size(); i++){
+            gc.drawImage(enemies.get(i), enemies.get(i).getX(), enemies.get(i).getY());
+        }
+
         levelUp();
 
     }
@@ -180,7 +244,7 @@ public class SpaceInvaders {
             gc.setFill(Color.rgb(r, g, b, opacity));
             gc.fillOval(posX, posY, w, h);
 
-            posY += 20;
+            posY += 5;
         }
     }
 
@@ -223,6 +287,8 @@ public class SpaceInvaders {
         static final int size = 6;
         int width= 24;
         int length = 48;
+
+        // Constructor
         public PlayerMissiles(int posX, int posY) {
             this.posX = posX;
             this.posY = posY;
@@ -242,28 +308,13 @@ public class SpaceInvaders {
         }
     }
 
-    public class Enemy extends SISprite {
-
-        public Enemy(int x, int y, int w, int h, String type, Image img) {
-            super(x, y, w, h, type, img);
-        }
-
-        public void drawEnemy() {
-            gc.drawImage(img, x, y,
-                    w, h);
-        }
-
-    }
-
-
     public void levelUp(){
-
         for (int j = 0; j < 5; j++) {
-            enemies.add(new Enemy(60 + j*WIDTH/5, 150, ENEMY_SIZE, ENEMY_SIZE, "enemy", ENEMY_BIG));
-            if (enemies.get(j).y > HEIGHT)
+            enemies.add(new SpriteAnimation(imageURLs.get("ENEMY_BIG"), ENEMY_SIZE, ENEMY_SIZE,
+                    60 + j * WIDTH/5, 150, true, true, 1, 1, "enemy" ));
+            if (enemies.get(j).getY() > HEIGHT)
                 enemies.remove(j);
         }
     }
-
 }
 
