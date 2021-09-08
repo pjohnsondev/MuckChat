8b3b6cf3c74f289c38144bab1b458f34dd614e90
¨Matthew <mtrippe@myune.edu.au>
2021-09-08 21:52:36 +1000

    tweaked battle.  left progress notes

--- client/src/main/java/muck/client/enduring_fantasy/Battle.java
+++ client/src/main/java/muck/client/enduring_fantasy/Battle.java
@@ -7,6 +7,7 @@ public class Battle {
     private int pcMag;
     private int pcStr;
     private int nextLvl;
+    private int pcLvl;
     private Player player;
 
     private int monsterHp;
@@ -18,21 +19,39 @@ public class Battle {
         this.pcMp = this.player.getMP();
         this.pcMag = this.player.getMagicStr();
         this.pcStr = this.player.getDamage();
-        this.nextLvl = this.player.getPlayerLvl();
+        this.pcLvl = this.player.getPlayerLvl();
+        //this.nextLvl = this.player.nextLevel();
     }
 
+    public void battleActions() {
+        System.out.println("*----------*");
+        //System.out.println("Name: " + this.player.getCharName()" HP: " + this.pcHp" MP: "
+                //+ this.pcMp" STR: " + this.pcStr + "\nMana: " + this.pcMag + " Lvl: " +
+                //this.pcLvl" Next Lvl: " + this.nextLvl);
+        /** Limit break? **/
+        System.out.println("*----------*");
+
+        /** Rest of action window code?**/
+    }
 
     public void battleInput(String input){
         /** Lets set up the action command window options  **/
-        if (input.equalsIgnoreCase("Attack")){
-            //this.pcAttack();
-        } else if (input.equalsIgnoreCase("Rest")){
-            //this.pcRest();
-        } else if (input.equalsIgnoreCase("Flee")){
-            //this.pcFlee();
-        }
+        if (input.equalsIgnoreCase("Attack")){ this.pcAttack();
+        } else if (input.equalsIgnoreCase("Rest")){ this.pcRest();
+        } else if (input.equalsIgnoreCase("Item")){ this.itemSelector();
+        } else if (input.equalsIgnoreCase("Check")){ this.pcCheckMon();
+        }// else if (input.equalsIgnoreCase("Flee")){ this.pcFlee();}
     }
 
+    /**
+    public void pcAttack(){ Place holder }
+
+    public void pcRest(){ Place holder }
+
+    public void itemSelector(){ Place holder }
+
+    public void pcCheckMon(){ Place holder }
+    **/
 }
 
 
