70885430fee73f70a2e2540d182f2cc2351c4821
Jesse Chow <jchow5@myune.edu.au>
2021-09-19 22:05:41 +1000

    Added comments to Sound

--- client/src/main/java/muck/client/Sound.java
+++ client/src/main/java/muck/client/Sound.java
@@ -1,18 +1,19 @@
 package muck.client;
 
-import java.nio.file.Paths;
-import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import java.io.File;
 
 
+// This class contains a mediaPlayer that allows sound to be played
 public class Sound {
 
     String soundPath;
     MediaPlayer mediaPlayer;
 
+    /**
+     * Constructor to create a sound object
+     * @param audio The sound path for the sound file to be played by the mediaPlayer
+     */
     public Sound(String audio) {
         this.soundPath = audio;
     }
@@ -21,10 +22,12 @@ public class Sound {
         return audio.soundPath;
     }
 
+    /**
+     * music Plays the sound file using mediaPlayer
+     * @param soundFile The path to the sound file to be played
+     */
     public void music(Sound soundFile) {
         try {
-//            String s = "src/main/resources/sounds/tilegame.mp3";
-
                 Media h = new Media(getClass().getResource(soundFile.soundPath).toExternalForm());
                 mediaPlayer = new MediaPlayer(h);
                 mediaPlayer.play();
