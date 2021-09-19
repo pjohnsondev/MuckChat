20d10394037a0a7c76ea9733efb6c679f69f94b1
Zach Swann <zswann@myune.edu.au>
2021-09-19 20:50:06 +1000

    Added Test Collusion for Sprite

--- client/src/test/java/muck/client/MapTest.java
+++ client/src/test/java/muck/client/MapTest.java
@@ -3,12 +3,14 @@ package muck.client;
 import javafx.scene.canvas.Canvas;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.mockito.Mockito;
 
-
-
+import java.awt.event.KeyEvent;
 
 /**
  * Tests concerning anything to do with the client-side map i.e checking players can't move out of bounds or into objects
@@ -34,18 +36,23 @@ public class MapTest {
         GameMap gm = new GameMap(canvas);
         logger.info("Test getTileIndex");
         assertEquals(gm.getTileIndex(20,30,1),0);
+
+
+
         //logger.info(gm.getTileIndex(350,350,2));
+
 
 
         // Test that an attempt to move off the game map fails (throws some exception)
     }
     @Test void testCollisionDetection() {
         // Test that an attempt to move into an object fails (throws some exception)
+        // Part of this test happens in the Sprite Test
     }
     @Test void testTileMapReader() {
         TileMapReader tm = new TileMapReader("/Test.tmx"); // This is Test/Resources
         //Make sure public methods in TileMapReader don't get changed.
-        assertEquals(tm.getWidth() , 80);
+        assertEquals(tm.getWidth() ,80);
         assertEquals(tm.getHeight() , 64);
         assertEquals(tm.getTileWidth() ,32);
         assertEquals(tm.getTileHeight() , 32);
