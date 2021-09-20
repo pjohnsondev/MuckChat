22450ea7b1d37a105f351440614ee4a31a259986
Paul Johnson <pjohnsondev@gmail.com>
2021-09-21 09:18:43 +1000

    Added comprehensive test for empty fields in singInTest

--- client/src/test/java/muck/client/SignInTest.java
+++ client/src/test/java/muck/client/SignInTest.java
@@ -3,35 +3,44 @@ package muck.client;
 import muck.client.controllers.SignInController;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class SignInTest {
-
-        private static final Logger logger = LogManager.getLogger(SignInTest.class);
-
-        @BeforeAll
-        public static void beforeAll(){
-            logger.info("Run this code 'before all' setup");
-        }
-
-        @BeforeEach
-        public void beforeEach(){
-            logger.info("Run this code 'before each' setup");
-        }
-
-        @Test
-        // TODO: check if Fields are empty
-        public void testIsNotEmpty(){
-            SignInController user = new SignInController();
-            assertEquals(true, user.isNotEmpty("username", "password"));
-        }
+import org.junit.jupiter.api.*;
+import org.testfx.framework.junit5.ApplicationTest;
 
 
+import static org.mockito.Mockito.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+public class SignInTest extends ApplicationTest {
+    private static final Logger logger = LogManager.getLogger(SignInTest.class);
+
+    @BeforeAll
+    public static void beforeAll(){
+        logger.info("Run this code 'before all' tests");
     }
+
+    @BeforeEach
+    public void beforeEach(){
+        logger.info("Run this code 'before each' test");
+    }
+
+    @Test
+    public void testIsNotEmpty() {
+        logger.info("Testing that empty input field check works");
+        SignInController user = spy(SignInController.class);
+        String testUsername = "Test Username";
+        String testPassword = "Test Password";
+        String testEmptyField = "";
+        doNothing().when(user).setError(anyString());
+        sleep(500);
+        assertAll(
+                () -> assertFalse(user.isNotEmpty(testUsername, testEmptyField)),
+                () -> assertFalse(user.isNotEmpty(testEmptyField, testPassword)),
+                () -> assertFalse(user.isNotEmpty(testEmptyField, testEmptyField)),
+                () -> assertTrue(user.isNotEmpty(testUsername, testPassword))
+        );
+    }
+
+}
 
