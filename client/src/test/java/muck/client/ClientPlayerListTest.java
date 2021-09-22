21e252685479b030a3312283afb0a580c0ce3159
Ashley Travaini <atravain@myune.edu.au>
2021-09-22 13:17:13 +1000

    Removed old library imports from ClientPlayerListTest

--- client/src/test/java/muck/client/ClientPlayerListTest.java
+++ client/src/test/java/muck/client/ClientPlayerListTest.java
@@ -7,7 +7,6 @@ import muck.core.character.Player;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
