bdfee802026a74709de4f23a0a59f2130c89f418
Peter John Kiefel <pkiefel@myune.edu.au>
2021-09-23 19:29:08 +1000

    Added Singleton code to Test Database

--- server/src/test/java/muck/server/testHelpers/TestDatabase.java
+++ server/src/test/java/muck/server/testHelpers/TestDatabase.java
@@ -3,6 +3,33 @@ package muck.server.testHelpers;
 import muck.server.database.Database;
 
 public class TestDatabase extends Database {
+    /** The single instance of the server that exists */
+    private static TestDatabase INSTANCE;
+
+    /** Gets the MuckServer object */
+    public static TestDatabase getINSTANCE() {
+        if (INSTANCE == null) {
+            System.out.println("Attempting to create Test Database");
+            INSTANCE = new TestDatabase();
+        }
+        else {
+            System.out.println("Test Database is up and running");
+        }
+        return INSTANCE;
+    }
+
+    public static boolean activeConnection(){
+        if (INSTANCE==null){
+            return false;
+        } else{
+            return true;
+        }
+    }
+
+    public static void setInstanceNull(){
+        INSTANCE = null;
+    }
+
     public TestDatabase () {
         super();
         this.dbName = "testDB";
